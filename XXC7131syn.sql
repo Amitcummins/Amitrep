dae972163eac2a345e4630c5bb05a4353cd08a46
Amitcummins <amit.kansara@cummins.com>
2016-08-23 16:02:23 +0530

    Add files via upload

---
+++ XXC7131syn.sql
@@ -0,0 +1,14 @@
+/******************************************************************************
+NAME:       XXC7131syn.sql
+PURPOSE:    DDL script for creation of public synonyms to support custom objects. 
+REVISIONS:
+Ver        Date        Author           Description
+---------  ----------  ---------------  ------------------------------------
+ 1.0        26-MAY-15   Infosys         1. Created the custom synonyms
+1.1           06-Nov-15     Infosys	Modified script for upgrade
+LIMITATIONS:
+NOTES:
+******************************************************************************/
+
+CREATE OR REPLACE SYNONYM XXC7131_SUPPLIER_DQ_OUT_SEQ FOR XXC.XXC7131_SUPPLIER_DQ_OUT_SEQ;
+
